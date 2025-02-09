f9623139dd3ffe4cfd019ca118a9b23ae59530a5
Yasin <asinarstanbekov51@gmail.com>
2025-02-09 23:21:02 +0600

    'g'

--- shop/urls.py
+++ shop/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from products.schema import schema
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
