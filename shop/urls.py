720b24ba3528db732c62683e47a579b61c7c6315
Yasin <asinarstanbekov51@gmail.com>
2025-02-09 23:33:41 +0600

    'g'

--- shop/urls.py
+++ shop/urls.py
@@ -4,11 +4,11 @@ from graphene_django.views import GraphQLView
 from products.schema import schema
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
+   path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
